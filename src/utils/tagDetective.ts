d7a680c6e9a9beb8e24366905c5a8d3ab43ce4a8
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-28 14:30:19 +0300

    feat: change display tag

--- src/utils/tagDetective.ts
+++ src/utils/tagDetective.ts
@@ -1,7 +1,8 @@
 const TagDetective = (str: string): string[] => {
     const regExp = /\B#\w+/gi
 
-    return str.match(regExp) || []
+    return str.match(regExp)?.map(tag => tag.slice(1)) || []
 }
+
 
 export default TagDetective
